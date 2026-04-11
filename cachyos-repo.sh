063f193f1983df869e1884466b84d8fa13e5fce5
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-04-12 02:26:10 +0400

    Bump mirrorlist version

--- cachyos-repo.sh
+++ cachyos-repo.sh
@@ -159,9 +159,9 @@ run_install() {
     local mirror_url="https://mirror.cachyos.org/repo/x86_64/cachyos"
 
     pacman -U "${mirror_url}/cachyos-keyring-20240331-1-any.pkg.tar.zst" \
-              "${mirror_url}/cachyos-mirrorlist-22-1-any.pkg.tar.zst"    \
-              "${mirror_url}/cachyos-v3-mirrorlist-22-1-any.pkg.tar.zst" \
-              "${mirror_url}/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst"  \
+              "${mirror_url}/cachyos-mirrorlist-27-1-any.pkg.tar.zst"    \
+              "${mirror_url}/cachyos-v3-mirrorlist-27-1-any.pkg.tar.zst" \
+              "${mirror_url}/cachyos-v4-mirrorlist-27-1-any.pkg.tar.zst"  \
               "${mirror_url}/pacman-7.1.0.r9.g54d9411-2-x86_64.pkg.tar.zst"
 
     local is_repo_added="$(check_if_repo_was_added)"
